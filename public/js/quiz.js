941d439e78cbd11be2eb5b115405683845651646
isacardosods <isabella.csantos@sptech.school>
2026-05-11 22:04:04 -0300

    feat: melhorias no front-end do quiz

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,4 +1,3 @@
-//retorna uma lista de elementos que possuem essa classe!!!
 const perguntas = document.querySelectorAll('.pergunta');
 const btn_proximo = document.getElementById('next_btn');
 const btn_anterior = document.getElementById('back_btn');
@@ -10,16 +9,10 @@ let pergunta_atual = 0;
 verificar();
 
 function verificar() {
-    if (pergunta_atual <= 0) {
-        btn_anterior.style.display = 'none';
-    } else {
-        btn_anterior.style.display = 'block';
-    }
+    btn_anterior.style.display = pergunta_atual <= 0 ? 'none' : 'block';
 
     if (pergunta_atual < perguntas.length) {
-
         perguntas[pergunta_atual].classList.add('ativa');
-
     } else {
         btn_proximo.style.display = 'none';
         btn_anterior.style.display = 'none';
@@ -28,44 +21,50 @@ function verificar() {
 }
 
 function avancar() {
+    const respostaAtual = document.querySelector(`input[name="q${pergunta_atual + 1}"]:checked`);
 
+    if (!respostaAtual) {
+        cardErro.style.display = "flex";
+        mensagem_erro.innerHTML = `Responda a questão atual antes de avançar!`;
+        return;
+    }
+
+    cardErro.style.display = "none";
     perguntas[pergunta_atual].classList.remove('ativa');
-
     pergunta_atual++;
-
     verificar();
 }
 
 function recuar() {
     perguntas[pergunta_atual].classList.remove('ativa');
-
     pergunta_atual--;
-
     verificar();
 }
 
 function capturarRespostas() {
     let questoes = [];
 
-    //checked retorna o elemento selecionado! nessa lógica, ele acessa o input atraves do for por conta do i++, que vai até 10 (pois existem 10 questões)
     for (let i = 1; i <= 10; i++) {
-
-        //guarda no vetor 'respostas' o id da alternativa
-        let resposta = document.querySelector(`input[name="q${i}"]:checked`);
+        const resposta = document.querySelector(`input[name="q${i}"]:checked`);
 
         if (!resposta) {
             cardErro.style.display = "flex";
-            mensagem_erro.innerHTML = `É necessário responder todas as questões!`
+            mensagem_erro.innerHTML = `É necessário responder todas as questões!`;
             return;
         }
 
-        let questao = {
-            "id": `q${i}`,
-            "resposta": `${resposta.id}`,
-            "tipo": `${resposta.value}`
-        };
-
-        questoes.push(questao);
+        questoes.push({ id: `q${i}`, resposta: resposta.id, tipo: resposta.value });
     }
-    console.log(questoes);
+
+    console.log('cadastro: ', questoes);
+    carregarDiv();
+}
+
+function carregarDiv() {
+    cardErro.style.display = "flex";
+    mensagem_erro.innerHTML = `Respostas enviadas! Redirecionando para Home...`;
+
+    setTimeout(() => {
+        window.location = '../index.html';
+    }, 2000);
 }
